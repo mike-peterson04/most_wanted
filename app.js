729249b4bec27cc64e105ffab540a2499d5b734a
unknown <mpeterson2004@gmail.com>
2021-04-08 11:48:13 -0600

    tableOut function working to recursively print out all people in array

--- app.js
+++ app.js
@@ -20,6 +20,22 @@ function searchByName(){
         console.log('Sorry, looks like there is no one with that name.');
     }
 }
-function tableOut(array){
+function tableOut(arr){
     //this function will output an array of objects as a table via dom manipulation
+    let result = document.getElementById("output")
+    let validate = arr.length
+    console.log(arr);
+    console.log(result);
+    result.innerHTML += "<table border=1>\
+    <tr>\
+    <td>First Name</td><td>Last Name</td><td>ID Number</td>\
+    </tr>\
+    <tr>\
+    <td>"+arr[0].firstName+"</td><td>"+arr[0].lastName+"</td><td>"+arr[0].id+"</td>\
+    </tr>\
+    </table>"
+    if (validate>1){
+        arr.shift();
+        tableOut(arr);
+    }
 }
